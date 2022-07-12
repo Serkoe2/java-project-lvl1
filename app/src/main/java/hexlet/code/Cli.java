3d67d7abb55b18d518cd3ec151ec4bf7f59cf80a
Тимошенков Сергей Валентинович <s.timoshenkov@tinkoff.ru>
2022-07-13 00:05:29 +0300

    add linter

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void Greeting() {
+    public static void greeting() {
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = sc.next();
