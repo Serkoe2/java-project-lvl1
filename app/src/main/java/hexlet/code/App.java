1a810027e820cfe7f5fe289d1a9c0992d47ca895
Тимошенков Сергей Валентинович <s.timoshenkov@tinkoff.ru>
2022-07-13 16:37:00 +0300

    commit

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,22 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n0 - Exit");
+        if (sc.hasNextInt()) {
+            choice = sc.nextInt();
+        }
+        System.out.println("Your choice: " + Integer.toString(choice) + "\n");
+        switch (choice) {
+            case 0:
+                return;
+            case 1:
+                Cli.greeting();
+        }
     }
 }
